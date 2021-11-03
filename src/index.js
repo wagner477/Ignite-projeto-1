b898ee510fcb88a4704898fdb04d7fac28c58d36
drk477 <61123555+drk477@users.noreply.github.com>
2021-11-02 23:00:10 -0300

    aula-4-completa

--- src/index.js
+++ src/index.js
@@ -28,4 +28,12 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  return res.json(customer.statement);
+});
+
 app.listen(3333);
