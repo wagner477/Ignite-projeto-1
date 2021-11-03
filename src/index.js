d9f1ea5ab9255216b411efd71a92d4165e99eb47
drk477 <61123555+drk477@users.noreply.github.com>
2021-11-02 23:21:40 -0300

    aula-6-completa

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -28,14 +44,8 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
-app.get("/statement", (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(400).json({ error: "Customer not found" });
-  }
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
 
   return res.json(customer.statement);
 });
